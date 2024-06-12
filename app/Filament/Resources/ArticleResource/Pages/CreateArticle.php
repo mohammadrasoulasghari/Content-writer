e14c637b73bd6c85650f5ecf1ca689be141de761
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-06-12 22:33:46 +0330

    create first blog sucess

--- app/Filament/Resources/ArticleResource/Pages/CreateArticle.php
+++ app/Filament/Resources/ArticleResource/Pages/CreateArticle.php
@@ -6,16 +6,33 @@ use App\Filament\Resources\ArticleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use JetBrains\PhpStorm\NoReturn;
+use OpenAI\Laravel\Facades\OpenAI;
 
 class CreateArticle extends CreateRecord
 {
     protected static string $resource = ArticleResource::class;
     #[NoReturn] protected function mutateFormDataBeforeCreate(array $data): array
     {
-        dd('hi');
+        $prompt = "
+        می خوام یه مقاله عالی و با جزئیات کامل و مفید برای خواننده برای من بنویسی.
+        موضوع مقاله: {$data['title']}
+        نوع مقاله: معرفی زبان یا تکنولوژی یا ابزار
+        لحن مقاله: صمیمی و خطاب به دوم شخص مفرد و با مثال های با مزه و جذاب برای نسل z (رعایت این لحن در تمام مقاله بسیار ضروری است)
+        اندازه مقاله: 5000 کلمه (لطفا با جزئیات و توضیحات کامل بنویس که اندازه مقاله حتما بالاتر از این تعداد کلمات باشد)
+        ";
 
-        $data['content'] = $response->json('choices')[0]['text'];
+        $response = OpenAI::chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                ['role' => 'user', 'content' => 'Hello!'],
+            ],
+        ]);
+        $data['content'] = $response->choices[0]->message->content;
 
         return $data;
+    }
+    protected function afterCreate(): void
+    {
+        $this->redirect(ArticleResource::getUrl('edit', ['record' => $this->record->getKey()]));
     }
 }
