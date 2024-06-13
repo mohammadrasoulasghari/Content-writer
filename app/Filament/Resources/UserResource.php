ad69b8c2cf07b127875763aa1c71cd176f1e0621
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-06-13 21:19:13 +0330

    add user management to system

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static ?string $label = 'کاربر';
+    protected static ?string $pluralLabel = 'کاربران';
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Card::make()->schema([
+                    TextInput::make('name')
+                        ->label('نام')
+                        ->required(),
+                    TextInput::make('email')
+                        ->label('ایمیل')
+                        ->required()
+                        ->email(),
+                    TextInput::make('password')
+                        ->label('رمز عبور')
+                        ->password()
+                        ->required()
+                        ->dehydrateStateUsing(fn ($state) => !empty($state) ? bcrypt($state) : null),
+                    DatePicker::make('email_verified_at')
+                        ->label('تاریخ تایید ایمیل')
+                        ->disabledOn('create'),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->label('نام')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('email')->label('ایمیل')->sortable()->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
