26e547c5b4ff1ffe4da4fdc7a3130d960e688f36
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-06-13 19:24:45 +0330

    add article fields

--- app/Filament/Resources/ArticleResource.php
+++ app/Filament/Resources/ArticleResource.php
@@ -5,10 +5,8 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\ArticleResource\Pages;
 use App\Filament\Resources\ArticleResource\RelationManagers;
 use App\Models\Article;
-use Filament\Forms;
+use Filament\Forms\Components\Card;
 use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
@@ -16,14 +14,12 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ArticleResource extends Resource
 {
     protected static ?string $model = Article::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
     protected static ?string $label = 'مقاله';
     protected static ?string $pluralLabel = 'مقالات';
 
@@ -31,26 +27,28 @@ class ArticleResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('title')
-                    ->label("عنوان مقاله")
-                    ->required()
-                    ->columnSpan(2),
-                Textarea::make('description')
-                    ->label("توضیحات ")
-                    ->columnSpan(2),
-                TagsInput::make('keywords')
-                    ->required()
-                    ->columnSpan(2)
-                    ->label('کلید واژه های  مقاله')
-                    ->placeholder('بعد از وارد کردن هر کلید واژه یه enter بزنید'),
-//                Repeater::make('members')
-//                    ->maxItems(3)
-//                    ->label("متون انگلیسی")
-//                    ->schema([
-//                        TextInput::make('متن')
-//                            ->columns(2),
-//                    ])
-//                    ->columnSpan(2)
+                Card::make()->schema([
+                    TextInput::make('title')
+                        ->label("عنوان مقاله")
+                        ->required()
+                        ->columnSpan(2),
+                    Textarea::make('description')
+                        ->label("توضیحات ")
+                        ->columnSpan(2),
+                    TagsInput::make('keywords')
+                        ->required()
+                        ->columnSpan(2)
+                        ->label('کلید واژه های  مقاله')
+                        ->placeholder('بعد از وارد کردن هر کلید واژه یه enter بزنید'),
+                    Repeater::make('english_texts')
+                        ->maxItems(3)
+                        ->label("متون انگلیسی")
+                        ->schema([
+                            Textarea::make('متن')
+                                ->columns(2),
+                        ])
+                        ->columnSpan(2)
+                ])
             ])
             ->columns(2);
     }
