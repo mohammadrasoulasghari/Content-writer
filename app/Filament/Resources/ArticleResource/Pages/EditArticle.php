afc10e05f764e9c2d684ef8a4247b839163defe5
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-06-13 20:06:27 +0330

    cistom edit article page

--- app/Filament/Resources/ArticleResource/Pages/EditArticle.php
+++ app/Filament/Resources/ArticleResource/Pages/EditArticle.php
@@ -4,11 +4,40 @@ namespace App\Filament\Resources\ArticleResource\Pages;
 
 use App\Filament\Resources\ArticleResource;
 use Filament\Actions;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\View;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\EditRecord;
 
 class EditArticle extends EditRecord
 {
     protected static string $resource = ArticleResource::class;
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                RichEditor::make('content')
+                    ->required()
+                    ->columnSpan('full')
+                    ->toolbarButtons([
+                        'attachFiles',
+                        'blockquote',
+                        'bold',
+                        'bulletList',
+                        'codeBlock',
+                        'h2',
+                        'h3',
+                        'italic',
+                        'link',
+                        'orderedList',
+                        'redo',
+                        'strike',
+                        'underline',
+                        'undo',
+                    ])->extraAttributes(['id' => 'rich-editor-content']),
+            ]);
+    }
 
     protected function getHeaderActions(): array
     {
