59088bc3a7f6455848aa1a0a315299ef1926a2ad
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-06-13 19:31:28 +0330

    add article and create success fully

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    use HasFactory;
+    protected $guarded = [];
+
+    protected $casts = [
+        'keywords' => 'array',
+        'english_texts' => 'array',
+    ];
 }
